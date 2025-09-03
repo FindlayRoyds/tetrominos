d9a9062a8242f99219490fb0a72671e60c9e3098
FindlayRoyds <findlayroyds@gmail.com>
2025-09-03 23:48:32 +1200

    automatically drop next piece with ARE delay

--- src/board.rs
+++ src/board.rs
@@ -1,19 +1,40 @@
 use bevy::{platform::collections::HashMap, prelude::*};
 
+use crate::{
+    tetromino::{Tetromino, TetrominoKind, is_tetromino_pos_valid},
+    try_unwrap,
+};
+
+pub struct BoardPlugin;
+
+impl Plugin for BoardPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Update, spawn_next_tetromino.in_set(TetrominoSpawning));
+    }
+}
+
+#[derive(SystemSet, Debug, Clone, Hash, Eq, PartialEq)]
+pub struct TetrominoSpawning;
+
 #[derive(Component)]
 pub struct Board {
     pub size: UVec2,
     pub tile_size: UVec2,
+    pub drop_delay: i32,
+    pub drop_delay_counter: i32,
 
     tiles: HashMap<IVec2, Entity>,
 }
 
 impl Board {
-    pub fn new(size: UVec2, tile_size: UVec2) -> Self {
+    pub fn new(size: UVec2, tile_size: UVec2, drop_delay: i32) -> Self {
         Self {
             size,
-            tiles: HashMap::new(),
             tile_size,
+            drop_delay,
+            drop_delay_counter: drop_delay,
+
+            tiles: HashMap::new(),
         }
     }
 
@@ -44,15 +65,60 @@ pub fn spawn_board(
     commands: &mut Commands,
     size: UVec2,
     tile_size: UVec2,
+    drop_delay: i32,
     meshes: &mut ResMut<Assets<Mesh>>,
     materials: &mut ResMut<Assets<ColorMaterial>>,
 ) {
     let rec_size = (size * tile_size).as_vec2();
     commands.spawn((
         Name::new("Board"),
-        Board::new(size, tile_size),
+        Board::new(size, tile_size, drop_delay),
         Mesh2d(meshes.add(Rectangle::new(rec_size.x, rec_size.y))),
         MeshMaterial2d(materials.add(Color::WHITE)),
         Transform::from_scale(vec3(4.0, 4.0, 4.0)),
     ));
+}
+
+fn spawn_next_tetromino(
+    mut commands: Commands,
+    mut boards: Query<(Entity, &mut Board)>,
+    tetrominoes: Query<&Tetromino>,
+) {
+    let (board_entity, mut board) = try_unwrap!(
+        boards.single_mut(),
+        "Expected one board when spawning tetromino"
+    );
+
+    for tetromino in tetrominoes.iter() {
+        if tetromino.board_entity == board_entity {
+            return; // Already a tetromino on the board
+        }
+    }
+
+    if board.drop_delay_counter > 0 {
+        board.drop_delay_counter -= 1;
+        return;
+    }
+    board.drop_delay_counter = board.drop_delay;
+
+    let kind = match fastrand::i32(0..7) {
+        0 => TetrominoKind::I,
+        1 => TetrominoKind::J,
+        2 => TetrominoKind::L,
+        3 => TetrominoKind::O,
+        4 => TetrominoKind::S,
+        5 => TetrominoKind::T,
+        _ => TetrominoKind::Z,
+    }; // TODO replace with that one crate idk the name
+    let pos = ivec2(4, board.size.y as i32);
+    if !is_tetromino_pos_valid(kind, 0, pos, &board) {
+        bevy::log::error!("Attempted to spawn tetromino at invalid position");
+        return;
+    }
+    commands.entity(board_entity).with_children(|parent| {
+        parent.spawn((
+            Name::new("Tetromino"),
+            Tetromino::new(kind, pos, 80, board_entity),
+        ));
+    });
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,9 @@
 use bevy::prelude::*;
 use bevy_inspector_egui::{bevy_egui::EguiPlugin, quick::WorldInspectorPlugin};
-use fastrand;
 
 use crate::{
     board::Board,
-    tetromino::{
-        Tetromino, TetrominoKind, TetrominoUpdates, is_tetromino_pos_valid, rotate_tetromino,
-    },
+    tetromino::{Tetromino, TetrominoUpdates, is_tetromino_pos_valid, rotate_tetromino},
     tile::{TileUpdates, TileVisuals},
 };
 
@@ -16,22 +13,30 @@ mod tile;
 mod warnings;
 
 use board::*;
+use tetromino::*;
+use tile::*;
 
 fn main() -> AppExit {
     App::new()
         .insert_resource(ClearColor(Color::srgb(0.0, 0.0, 0.0)))
         .add_plugins((DefaultPlugins.set(ImagePlugin::default_nearest()),))
         .add_plugins((EguiPlugin::default(), WorldInspectorPlugin::new()))
-        .add_plugins((tile::TilePlugin, tetromino::TetrominoPlugin))
+        .add_plugins((TilePlugin, TetrominoPlugin, BoardPlugin))
         .add_systems(Startup, setup)
         .add_systems(
             Update,
-            (
-                (handle_keypress).in_set(TileUpdates),
-                (rotate_tetrominoes, move_tetrominoes).before(TetrominoUpdates),
-            ),
+            (rotate_tetrominoes, move_tetrominoes).before(TetrominoUpdates),
         )
-        .configure_sets(Update, (TetrominoUpdates, TileUpdates, TileVisuals).chain())
+        .configure_sets(
+            Update,
+            (
+                TetrominoSpawning,
+                TetrominoUpdates,
+                TileUpdates,
+                TileVisuals,
+            )
+                .chain(),
+        )
         .run()
 }
 
@@ -46,43 +51,10 @@ fn setup(
         &mut commands,
         uvec2(10, 20),
         uvec2(8, 8),
+        40,
         &mut meshes,
         &mut materials,
     );
-}
-
-fn handle_keypress(
-    mut commands: Commands,
-    keyboard: Res<ButtonInput<KeyCode>>,
-    boards: Query<(Entity, &Board)>,
-) {
-    if keyboard.just_pressed(KeyCode::Space) {
-        let (board_entity, board) = try_unwrap!(
-            boards.single(),
-            "Expected one board when spawning tetromino"
-        );
-
-        let kind = match fastrand::i32(0..7) {
-            0 => TetrominoKind::I,
-            1 => TetrominoKind::J,
-            2 => TetrominoKind::L,
-            3 => TetrominoKind::O,
-            4 => TetrominoKind::S,
-            5 => TetrominoKind::T,
-            _ => TetrominoKind::Z,
-        };
-        let pos = ivec2(4, board.size.y as i32);
-        if !is_tetromino_pos_valid(kind, 0, pos, board) {
-            bevy::log::error!("Attempted to spawn tetromino at invalid position");
-            return;
-        }
-        commands.entity(board_entity).with_children(|parent| {
-            parent.spawn((
-                Name::new("Tetromino"),
-                Tetromino::new(kind, pos, 80, board_entity),
-            ));
-        });
-    }
 }
 
 fn move_tetrominoes(
